a243ae03a0529d918af26e5f38ce88d189578a08
ritesh goyal <goyalritesh20@gmail.com>
2021-02-09 12:14:36 +0530

    added user agent profile

--- accounts/views.py
+++ accounts/views.py
@@ -9,12 +9,48 @@ User = get_user_model()
 from django.urls import reverse
 from accounts.forms import UserForm
 from accounts.tasks import send_mail_new_user_register, send_mail_for_reset_password
+from django.utils import timezone
 
 # Create your views here.
 
 @login_required
 def home_view(request):
-    return render(request,'home.html',{})
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+
+    device_type = ""
+    browser_type = ""
+    browser_version = ""
+    os_type = ""
+    os_version = ""
+
+    if request.user_agent.is_mobile:
+        device_type = "Mobile"
+    if request.user_agent.is_tablet:
+        device_type = "Tablet"
+    if request.user_agent.is_pc:
+        device_type = "PC"
+
+    browser_type = request.user_agent.browser.family
+    browser_version = request.user_agent.browser.version_string
+    os_type = request.user_agent.os.family
+    os_version = request.user_agent.os.version_string
+
+
+    context = {
+        "ip": ip,
+        "device_type": device_type,
+        "browser_type": browser_type,
+        "browser_version": browser_version,
+        "os_type":os_type,
+        "os_version":os_version,
+        "access_at": timezone.now().strftime("%B %d, %Y %H:%M:%S %Z(%z)")
+    }
+
+    return render(request,'home.html',context)
 
 def user_login(request):
     if request.method == 'POST':
--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
+    'django_user_agents',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_user_agents.middleware.UserAgentMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
